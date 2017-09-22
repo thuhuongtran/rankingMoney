a0cc11957a3a291669704cd90e4f9142729fa495
thuhuong <thuhuong@DESKTOP-NDT0E4B.net.fpt>
2017-09-22 12:09:03 +0700

    config to connect to db

---
+++ src/main/java/com/misa/ranking/pools/HikariPool.java
@@ -0,0 +1,27 @@
+package com.misa.ranking.pools;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class HikariPool {
+    private static final String HIRAKI_CONFIG_FILE = "config/hikari.properties";
+    private static HikariDataSource dataSource;
+    public static void init() throws IOException {
+        InputStream input = new FileInputStream(HIRAKI_CONFIG_FILE);
+        Properties prop = new Properties();
+        prop.load(input);
+        HikariConfig config = new HikariConfig(prop);
+        config.setMaximumPoolSize(20);
+        dataSource = new HikariDataSource(config);
+    }
+    public static Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
+}
